235c0cbafdb133a2a9f111bde1bb1e30ae09ab29
Rafał Sobieraj <46581279+RafalSobieraj@users.noreply.github.com>
2022-01-29 01:58:04 +0100

    Update Startup.cs

--- WebsiteProject/Startup.cs
+++ WebsiteProject/Startup.cs
@@ -27,8 +27,42 @@ namespace WebsiteProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connectionString = Configuration.GetConnectionString("Default");
-            services.AddDbContext<MyDbContext>(options => options.UseMySQL(connectionString));
+            services.AddDbContext<DbContext>(x =>
+            {
+
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                string connStr;
+
+                if (env == "Development")
+                {
+                    connStr = Configuration.GetConnectionString("ConfigurationDB");
+
+
+                }
+                else
+                {
+                    // Use connection string provided at runtime by Heroku.
+                    var connUrl = Environment.GetEnvironmentVariable("CLEARDB_DATABASE_URL");
+
+                    connUrl = connUrl.Replace("mysql://", string.Empty);
+                    var userPassSide = connUrl.Split("@")[0];
+                    var hostSide = connUrl.Split("@")[1];
+
+                    var connUser = userPassSide.Split(":")[0];
+                    var connPass = userPassSide.Split(":")[1];
+                    var connHost = hostSide.Split("/")[0];
+                    var connDb = hostSide.Split("/")[1].Split("?")[0];
+
+
+                    connStr = $"server={connHost};Uid={connUser};Pwd={connPass};Database={connDb}";
+
+
+
+                }
+
+                x.UseMySql(connStr);
+
+            });
             services.AddControllersWithViews();
         }
 
