735fda095471be98a1b72618fbda57a8b786efb0
Rafał Sobieraj <46581279+RafalSobieraj@users.noreply.github.com>
2022-01-29 02:12:59 +0100

    Update Startup.cs

--- WebsiteProject/Startup.cs
+++ WebsiteProject/Startup.cs
@@ -27,7 +27,7 @@ namespace WebsiteProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DbContext>(x =>
+            services.AddDbContext<MyDbContext>(x =>
             {
 
                 var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
